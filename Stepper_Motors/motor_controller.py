8a1587e36edd6721910e7a9d503ecc414ced87b5
ChrisBastajian <chrisbastajian@gmail.com>
2025-11-04 14:52:05 -0500

    serial communication for stepper control

--- Stepper_Motors/motor_controller.py
+++ Stepper_Motors/motor_controller.py
@@ -9,9 +9,9 @@ def send_to_c_program(message):
     time.sleep(2) #wait for the connection to establish
     ser.write((str(message)+"\n").encode())
 
-def continuous_servo_rotation(time, angle, servo_number=1):
+def continuous_servo_rotation(rot_time, angle, servo_number=1):
     #servo # 1 = rotation | servo#2 = translation
     send_to_c_program(f"{servo_number},{time},{angle}")
 
 #testing script:
-continuous_servo_rotation(time=10, angle=180, servo_number=1)
+continuous_servo_rotation(rot_time=10, angle=180, servo_number=1)
