845c64d6a3d64eacacdb510487c142c084410725
ChrisBastajian <chrisbastajian@gmail.com>
2025-11-15 16:12:03 -0500

    send to serial with specific usb port

--- Stepper_Motors/motor_controller.py
+++ Stepper_Motors/motor_controller.py
@@ -8,12 +8,12 @@ def send_to_c_program(usb_port, message):
 
 def continuous_stepper_rotation(rot_time, angle, stepper_number=1, usb_port='COM1'):
     #stepper # 1 = rotation | stepper#2 = translation
-    send_to_c_program(usb_port,f"{stepper_number},{angle},{rot_time}")
+    send_to_c_program(usb_port,f"{stepper_number},{angle},{rot_time},f0\n")
 
 def continuous_double_rotation(rot_time, angle_xy, angle_z, usb_port = 'COM1'):
-    send_to_c_program(usb_port,f"{angle_xy},{angle_z},{rot_time}")
+    send_to_c_program(usb_port,f"{angle_xy},{angle_z},{rot_time},f1\n")
 
-def read_serial(port,baudrate):
+def read_serial(port, baudrate):
      ser = serial.Serial(port, baudrate, timeout=1)
      data_raw = ser.readline()
      if data_raw:
